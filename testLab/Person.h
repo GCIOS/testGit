30581e0b18e02cdc68a2443a7f84a9d400cb3228
gaolaoshi <gaochong443@163.com>
2019-11-21 10:16:23 +0800

    perosn

---
+++ testLab/Person.h
@@ -0,0 +1,17 @@
+//
+//  Person.h
+//  testLab
+//
+//  Created by gao on 2019/11/21.
+//  Copyright © 2019 HLT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Person : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ testLab/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  testLab
+//
+//  Created by gao on 2019/11/21.
+//  Copyright © 2019 HLT. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
