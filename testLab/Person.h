de2b6b19acd08a3e1330888bb57ef6075c1acc5f
gaolaoshi <gaochong443@163.com>
2019-12-27 11:05:50 +0800

    person name

---
+++ testLab/Person.h
@@ -0,0 +1,17 @@
+//
+//  Person.h
+//  testLab
+//
+//  Created by gao on 2019/12/27.
+//  Copyright © 2019 HLT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Person : NSObject
+@property (nonatomic, copy) NSString *name;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ testLab/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  testLab
+//
+//  Created by gao on 2019/12/27.
+//  Copyright © 2019 HLT. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
