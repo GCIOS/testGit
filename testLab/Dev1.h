a6444b20ff3f2706f32427f2780b6ef315d1bcd2
gaolaoshi <gaochong443@163.com>
2019-12-27 09:57:32 +0800

    dev1

---
+++ testLab/Dev1.h
@@ -0,0 +1,17 @@
+//
+//  Dev1.h
+//  testLab
+//
+//  Created by gao on 2019/12/27.
+//  Copyright © 2019 HLT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Dev1 : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ testLab/Dev1.m
@@ -0,0 +1,13 @@
+//
+//  Dev1.m
+//  testLab
+//
+//  Created by gao on 2019/12/27.
+//  Copyright © 2019 HLT. All rights reserved.
+//
+
+#import "Dev1.h"
+
+@implementation Dev1
+
+@end
